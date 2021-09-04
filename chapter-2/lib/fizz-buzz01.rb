f24dc55b71475fac8d69980bbbcdbc723e51d9b3
Midori-Gift <okometaberoyo5009@gmail.com>
2021-09-04 10:19:19 +0900

    fizzbuzz作作成完了

---
+++ chapter-2/lib/fizz-buzz01.rb
@@ -0,0 +1,17 @@
+n = gets
+
+n = n.to_i
+
+def fizz_buzz(n)
+  if(n % 15 == 0)
+    puts "FizzBuzz"
+  elsif(n % 5 == 0)
+    puts "Fizz"
+  elsif(n % 3 == 0)
+    puts"Buzz"
+  else
+    puts n
+  end
+end
+
+puts fizz_buzz(n)
